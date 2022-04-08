b8c84205bf743c2505a12c94fc8a55caefa349ed
Josh Ribakoff <jribakoff@secondmeasure.com>
2022-04-08 09:33:11 -0700

    reset bug

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
 import "./App.css";
-import { useQuery } from "react-query";
-import { useMemo, useTransition, useState, useEffect } from "react";
+import { useQueryErrorResetBoundary,useQuery } from "react-query";
+import { useMemo, useTransition, useState, useEffect, Suspense } from "react";
+import {ErrorBoundary} from 'react-error-boundary'
 
 function App() {
-  const [variables, setVariables] = useState(() => Math.random());
-  const [isPending, startTransition] = useTransition();
-  
-  const result = useQuery(
-    ["foo", variables],
-    async () => {
-      console.log("query ran", variables);
-      await new Promise((res) => setTimeout(res, 1000));
-      return () => "foo" + variables;
-    },
-    { suspense: true, staleTime: Infinity, cacheTime: 0 }
-  );
-  
-  const data = useMemo(() => {
-    console.log("data changed", result.data);
-    return result.data;
-  }, [result.data]);
-  
-  useEffect(() => {
-    console.log("mount");
-    return () => console.log("unmount");
-  });
-
-  console.log(result);
+  const { reset } = useQueryErrorResetBoundary()
+  const [resetKey, setResetKey] = useState(0)
   return (
     <div className="App">
-      {JSON.stringify(result)}
-      <button
-        onClick={() => {
-          startTransition(() => {
-            setVariables(Math.random());
-          });
-        }}
-      >
-        fetch
-      </button>
+      <Suspense fallback={<>loading fallback</>}>
+        <ErrorBoundary fallback={<>error fallback</>} resetKeys={[resetKey]}>
+        <ComponentWithData />
+        </ErrorBoundary>
+      </Suspense>
+      <br />
+      <button onClick={()=>{
+        reset()
+        return setResetKey(Math.random());
+      }}>reset error boundary</button>
     </div>
   );
+}
+
+function ComponentWithData() {
+  const result = useQuery(
+    "foo",
+    async () => {
+      console.log("query ran");
+      return Promise.reject('rejected by Anthony Davis')
+    },
+    { retry: false, staleTime: Infinity, cacheTime: 0 }
+  )
+  console.log(result);
 }
 
 export default App;
